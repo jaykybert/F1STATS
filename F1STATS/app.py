8c7641b71d6cc29a5b7d93841176ae0df05ca79b
jaykybert <51761831+jaykybert@users.noreply.github.com>
2019-06-23 21:18:09 +0100

    Seperate standing pages, added home page.

--- F1STATS/app.py
+++ F1STATS/app.py
@@ -7,15 +7,23 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/index')
 def index():
+    return render_template('index.html')
 
-    return render_template('index.html', d_standings=d, c_standings=c)
+
+@app.route('/driver-standings')
+def driver_standings():
+    return render_template('driver_standings.html', d_standings=d)
+
+
+@app.route('/constructor-standings')
+def constructor_standings():
+    return render_template('constructor_standings.html', c_standings=c)
 
 
 global d, c
 d = current_standings.current_driver_standings()
 c = current_standings.current_constructor_standings()
 
+
 if __name__ == '__main__':
     app.run()
-
-
--- F1STATS/current_standings.py
+++ F1STATS/current_standings.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import pprint
 
 """Consider an additional information button?
 Gives number of wins, driver numbers, etc.
@@ -16,33 +16,33 @@ def current_driver_standings():
     response = requests.get(url)
     if not response.ok:
         return []
-    else:
-        data = json.loads(response.text)
 
-        standings_list = []
-        driver_list = []
-        for driver in data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']:
-            fn = driver['Driver']['givenName']
-            ln = driver['Driver']['familyName']
-            nation = driver['Driver']['nationality']
-            url = driver['Driver']['url']
-            cons = driver['Constructors'][0]['name']
-            points = driver['points']
-            pos = driver['position']
-            wins = driver['wins']
+    data = json.loads(response.text)
 
-            driver_info = {'fn': fn, 'ln': ln, 'url': url, 'points': points,
-                           'pos': pos, 'wins': wins, 'nationality': nation,
-                           'cons': cons}
+    standings_list = []
+    driver_list = []
+    for driver in data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings']:
+        fn = driver['Driver']['givenName']
+        ln = driver['Driver']['familyName']
+        nation = driver['Driver']['nationality']
+        url = driver['Driver']['url']
+        cons = driver['Constructors'][0]['name']
+        points = driver['points']
+        pos = driver['position']
+        wins = driver['wins']
 
-            driver_list.append(driver_info)
+        driver_info = {'fn': fn, 'ln': ln, 'url': url, 'points': points,
+                       'pos': pos, 'wins': wins, 'nationality': nation,
+                       'cons': cons}
+
+        driver_list.append(driver_info)
             
-        standings_list.append(driver_list)
-        round_n = data['MRData']['StandingsTable']['StandingsLists'][0]['round']
-        season = data['MRData']['StandingsTable']['StandingsLists'][0]['season']
-        standings_list.append({'round': round_n, 'season': season})
+    standings_list.append(driver_list)
+    round_n = data['MRData']['StandingsTable']['StandingsLists'][0]['round']
+    season = data['MRData']['StandingsTable']['StandingsLists'][0]['season']
+    standings_list.append({'round': round_n, 'season': season})
         
-        return standings_list
+    return standings_list
 
 
 def current_constructor_standings():
@@ -51,27 +51,26 @@ def current_constructor_standings():
     response = requests.get(url)
     if not response.ok:
         return []
-    else:
-        data = json.loads(response.text)
 
-        standings_list = []
-        con_list = []
-        for con in data['MRData']['StandingsTable']['StandingsLists'][0]['ConstructorStandings']:
-            name = con['Constructor']['name']
-            nation = con['Constructor']['nationality']
-            url = con['Constructor']['url']
+    data = json.loads(response.text)
 
-            points = con['points']
-            pos = con['position']
-            wins = con['wins']
+    standings_list = []
+    con_list = []
+    for con in data['MRData']['StandingsTable']['StandingsLists'][0]['ConstructorStandings']:
+        name = con['Constructor']['name']
+        nation = con['Constructor']['nationality']
+        url = con['Constructor']['url']
+        points = con['points']
+        pos = con['position']
+        wins = con['wins']
 
-            con_info = {'name': name, 'nationality': nation, 'url': url,
-                        'points': points, 'pos': pos, 'wins': wins}
-            con_list.append(con_info)
+        con_info = {'name': name, 'nationality': nation, 'url': url,
+                    'points': points, 'pos': pos, 'wins': wins}
+        con_list.append(con_info)
             
-        standings_list.append(con_list)
-        round_no = data['MRData']['StandingsTable']['StandingsLists'][0]['round']
-        season = data['MRData']['StandingsTable']['StandingsLists'][0]['season']
-        standings_list.append({'round': round_no, 'season': season})
+    standings_list.append(con_list)
+    round_no = data['MRData']['StandingsTable']['StandingsLists'][0]['round']
+    season = data['MRData']['StandingsTable']['StandingsLists'][0]['season']
+    standings_list.append({'round': round_no, 'season': season})
 
-        return standings_list
+    return standings_list
